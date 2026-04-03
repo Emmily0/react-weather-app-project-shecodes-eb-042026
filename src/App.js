330ea2dd93517c64b668740aa898b2638040124b
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-03 17:57:35 +0200

    Added footer with updated links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-       <h1>React Weather App</h1>
+       React Weather App
+       <footer>
+      <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer" title="SheCodes WeatherApp React Project">SheCodes</a> 
+  React Project coded in 2026 by <a href="https://www.shecodes.io/graduates/15505-emily-berner" target="_blank" rel="noreferrer" title="Emily's SheCodes profile"><strong>Emily Berner</strong></a>
+  , open-sourced on <a href="https://github.com/Emmily0/react-weather-app-project-shecodes-eb-042026" target="_blank" rel="noreferrer" title="Emily's WeatherApp React Project on GitHub">GitHub</a> 
+  and hosted on <a href="https://react-weatherapp-p-shecodes-eb-042026.netlify.app/" target="_blank" rel="noreferrer" title="Emily's WeatherApp React Project on Netlify">Netlify</a> 
+  </footer>
     </div>
   );
 };
