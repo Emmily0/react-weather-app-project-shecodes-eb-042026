33596471e22ae09b46ee26210f7f2882aad558fa
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-03 18:48:15 +0200

    Added Weather and Search HTML structure

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,45 @@ import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div className="Weather">React Weather App</div>
+        <div className="Weather">
+            <form>
+                <div className="row">
+                <div className="col-9">
+                <input 
+                type="search" 
+                placeholder="Enter a city..."
+                className="form-control"/>
+                </div>
+                <div className="col-3">
+                <input
+                type="sbumit"
+                value="Search"
+                className="btn btn-primary"
+                />
+                </div>
+                </div>
+            </form>
+            <h1>New York</h1>
+            <ul>
+                <li>Friday 12:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img
+                    src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
+                    alt="Mostly Cloudy"/>
+                12°C
+                </div>
+                <div className="col-6">
+                <ul>
+                    <li>Precipitation: 10%</li>
+                    <li>Humidity: 87%</li>
+                    <li>Wind: 11 km/h</li>
+                </ul>                
+                </div>
+            </div>
+
+        </div>
     );
 }
