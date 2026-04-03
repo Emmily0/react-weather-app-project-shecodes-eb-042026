6c25698e5fbad84dc31315ba2f60797b5326e906
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-03 23:24:00 +0200

    Added CSS to my Project

--- src/Weather.js
+++ src/Weather.js
@@ -10,13 +10,14 @@ export default function Weather() {
                 <input 
                 type="search" 
                 placeholder="Enter a city..."
-                className="form-control"/>
+                className="form-control"
+                autoFocus="on"/>
                 </div>
                 <div className="col-3">
                 <input
                 type="sbumit"
                 value="Search"
-                className="btn btn-primary"
+                className="btn btn-primary w-100"
                 />
                 </div>
                 </div>
@@ -26,12 +27,16 @@ export default function Weather() {
                 <li>Friday 12:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
+                    <div className="d-flex align-items-start">
                     <img
                     src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                    alt="Mostly Cloudy"/>
-                12°C
+                    alt="Mostly Cloudy"
+                    />
+                    <span className="temperature">12</span>
+                    <span className="unit">°C</span>
+                    </div>
                 </div>
                 <div className="col-6">
                 <ul>
